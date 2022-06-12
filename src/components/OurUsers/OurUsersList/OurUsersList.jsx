9340a61dab0d62ad796263225a1c76165b667d6d
vladho <vladhopilo@gmail.com>
2022-06-12 22:28:32 +0300

    done

--- src/components/OurUsers/OurUsersList/OurUsersList.jsx
+++ src/components/OurUsers/OurUsersList/OurUsersList.jsx
@@ -1,11 +1,22 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-// import { getUsers } from "../../../redux/users/usersOperation";
 import { getAllUsers } from "../../../redux/users/usersSelectors";
+import OurUsersItems from "../OurUsersItems/OurUsersItems";
+import styles from "./OurUsersList.module.scss";
+import Button from "../../common/Button/Button";
 
 const OurUsersList = () => {
   const users = useSelector(getAllUsers);
-  return <></>;
+  return (
+    <>
+      <ul className={styles.list}>
+        {users.map((el) => (
+          <OurUsersItems key={el.id} users={el} />
+        ))}
+      </ul>
+      <Button className={styles.btn}>Show more</Button>
+    </>
+  );
 };
 
 export default OurUsersList;
